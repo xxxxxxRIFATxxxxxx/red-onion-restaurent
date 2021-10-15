e17cb9b389990c45668ff0875f4b84acdf510eec
Md. Rifat Islam <xxxxxxrifatxxxxxx@gmail.com>
2021-10-15 12:08:07 +0600

    Sign Up Page Done

--- src/App.js
+++ src/App.js
@@ -2,14 +2,22 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AuthProvider from './context/AuthProvider';
 import Signup from './components/Signup/Signup';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <AuthProvider>
-        <Signup>
-        </Signup>
-      </AuthProvider>
+      <Router>
+        <Switch>
+          <Route path="/signup">
+            <Signup></Signup>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,29 +1,40 @@
-import Button from '@restart/ui/esm/Button';
 import React from 'react';
 import { Form } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
+import './Signup.css';
+import logo from '../../images/logo-black.png';
 
 const Signup = () => {
     return (
-        <Form>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
-                <Form.Text className="text-muted">
-                    We'll never share your email with anyone else.
-                </Form.Text>
-            </Form.Group>
+        <div className="form small-font">
+            <div className="text-center pt-5">
+                <img className="img-fluid logo" src={logo} alt="red onion logo" />
+            </div>
 
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                <Form.Check type="checkbox" label="Check me out" />
-            </Form.Group>
-            <Button variant="primary" type="submit">
-                Submit
-            </Button>
-        </Form>
+            <Form className="container mx-auto">
+                <Form.Group className="mb-3" controlId="formBasicName">
+                    <Form.Control className="silver-bg text-muted small-font border-1 p-3" type="text" placeholder="Name" />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Control className="silver-bg text-muted small-font border-1 p-3" type="email" placeholder="Email" />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Control className="silver-bg text-muted small-font border-1 p-3" type="password" placeholder="Password" />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicConfirmPassword">
+                    <Form.Control className="silver-bg text-muted small-font border-1 p-3" type="password" placeholder="Confirm Password" />
+                </Form.Group>
+
+                <button className="btn btn-danger w-100 small-font p-3 mb-3">Sign up</button>
+
+                <div className="text-center">
+                    <NavLink to="/login" className="text-danger text-decoration-none small-font">Already have an account</NavLink>
+                </div>
+            </Form>
+        </div>
     );
 };
 
